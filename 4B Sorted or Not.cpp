6a46f5bf1c3a3e9ac1adf9ab1c6fed9593fc844f
bunnerman <parnilrbalawade@gmail.com>
2026-03-02 23:06:54 +0530

    Create 4B Sorted or Not.cpp

---
+++ 4B Sorted or Not.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter number of elements: ";
+	cin >> n;
+	cout << "Enter " << n << " elements: ";
+	int ary[n];
+	for (int i = 0; i < n; i++)
+		cin >> ary[i];
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (ary[i] > ary[i + 1])
+		{
+			cout << "Unsorted List";
+			return 0; 
+		}
+	}
+
+	cout << "Sorted List";
+
+	return 0;		
+}
