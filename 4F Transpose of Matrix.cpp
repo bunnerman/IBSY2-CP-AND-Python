d977f136980e7f8656dac028dec430cae294a9f2
bunnerman <parnilrbalawade@gmail.com>
2026-03-03 15:09:39 +0530

    Create 4F Transpose of Matrix.cpp

---
+++ 4F Transpose of Matrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int r, c;
+	int n;
+	
+	cout << "Enter rows x columns: ";
+	cin >> r;
+	cin >> c;
+	int mtrx[r][c];
+	int tMtrx[c][r];
+	cout << "Enter " << r * c << " elements: ";
+	for (int i = 0; i < r; i++)
+		for (int j = 0; j < c; j++)
+		{
+			cin >> mtrx[i][j];
+			tMtrx[j][i] = mtrx[i][j];
+		}
+
+	for (int i = 0; i < c; i++)
+	{
+		for (int j = 0; j < r; j++)
+			cout << tMtrx[i][j] << " ";
+		cout << "\n";
+	}
+	return 0;
+}
