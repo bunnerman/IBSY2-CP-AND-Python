e6f0c207ac68620ee4ad271bae037c1cc59750fa
bunnerman <parnilrbalawade@gmail.com>
2026-03-03 18:47:51 +0530

    Update 4F Transpose of Matrix.cpp

--- 4F Transpose of Matrix.cpp
+++ 4F Transpose of Matrix.cpp
@@ -9,19 +9,15 @@ int main()
 	cin >> r;
 	cin >> c;
 	int mtrx[r][c];
-	int tMtrx[c][r];
 	cout << "Enter " << r * c << " elements: ";
 	for (int i = 0; i < r; i++)
 		for (int j = 0; j < c; j++)
-		{
 			cin >> mtrx[i][j];
-			tMtrx[j][i] = mtrx[i][j];
-		}
 
 	for (int i = 0; i < c; i++)
 	{
 		for (int j = 0; j < r; j++)
-			cout << tMtrx[i][j] << " ";
+			cout << mtrx[j][i] << " ";
 		cout << "\n";
 	}
 	return 0;
