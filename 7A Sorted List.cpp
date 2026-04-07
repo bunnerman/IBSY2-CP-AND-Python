1e97e47b210e4fb95f5f511f32fced9fe5bf52e3
bunnerman <parnilrbalawade@gmail.com>
2026-04-07 22:36:23 +0530

    Create 7A Sorted List.cpp

---
+++ 7A Sorted List.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+
+int main()
+{
+	int n = 5;
+	std::vector<int> vec(n);
+	for (int &i: vec)
+		std::cin >> i;
+	std::sort(vec.begin(), vec.end());
+	for (int &i : vec)
+		std::cout << i << " ";
+
+	return 0;
+}
